1270431aee4c3a8d023ecb4d40f16fe5e819850b
Myrka Larios <myrka@michelada.io>
2019-04-18 17:18:02 -0500

    Login and stuff

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
 gem 'tiny_tds'
 gem 'activerecord-sqlserver-adapter'
+gem "jquery-rails"
 
 # Background Jobs Adapter
 gem "sidekiq"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'dashboard#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
